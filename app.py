0e1acd3b9988533d2847bb55878050ebd522c7eb
sushant.kumar <sushant3660jee@gmail.com>
2025-07-01 17:03:14 +0530

    batch_embedding and batch_masking added

--- app.py
+++ app.py
@@ -30,10 +30,48 @@ def embed_message():
         message = data.get('message', '')
         embedding = get_embedding(message)
         print("Generated embedding")
-        # print(embedding.shape)
         return jsonify({"embedding": embedding})
     except Exception as e:
         return jsonify({"error": str(e)}), 400
+    
+@app.route('/embed_batch', methods=['POST'])
+def embed_batch():
+    try:
+        data = request.get_json()
+        batches = data.get("batches", [])  # List of dicts with ticket_id and message
+
+        results = []
+        for item in batches:
+            ticketId = item.get("ticketId")
+            message = item.get("message", "")
+            embedding = get_embedding(message)
+            results.append({
+                "ticketId": ticketId,
+                "embedding": embedding
+            })
+
+        return jsonify(results)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+
+@app.route('/mask_batch', methods=['POST'])
+def mask_batch():
+    try:
+        data = request.get_json()
+        batches = data.get('batches', [])  # List of {"ticket_id": ..., "messages": [...]}
+
+        results = []
+        for item in batches:
+            ticketId = item.get("ticketId")
+            messages = item.get("messages", [])
+            masked = [full_masking_pipeline(m) for m in messages if m.strip()]
+            results.append({"ticketId": ticketId, "maskedMessages": masked})
+        return jsonify(results)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+    
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
